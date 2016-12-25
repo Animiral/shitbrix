db6e62d4e04a827b7393d8ef492878c8c7b55b05
Peter Neubauer <peterneubauer2@gmail.com>
2016-12-25 01:48:42 +0100

    Physical: implement all unit tests, close #39

--- tests/test_pit.cpp
+++ tests/test_pit.cpp
@@ -39,7 +39,7 @@ bool contains_n(const Pit& pit, size_t blocks, size_t garbages)
 	for(auto& physical : pit.contents()) {
 		if(dynamic_cast<Block*>(&*physical))
 			actual_blocks++;
-		else if(dynamic_cast<Block*>(&*physical))
+		else if(dynamic_cast<Garbage*>(&*physical))
 			actual_garbages++;
 	}
 
@@ -103,7 +103,10 @@ TEST_F(PitTest, SpawnBlock)
  */
 TEST_F(PitTest, SpawnBlockOutOfBounds)
 {
-	FAIL();
+	RowCol red_rc{1, -1};
+	RowCol green_rc{3, 6};
+	EXPECT_THROW(pit->spawn_block(Block::Color::RED, red_rc, Block::State::REST), GameException);
+	EXPECT_THROW(pit->spawn_block(Block::Color::GREEN, green_rc, Block::State::REST), GameException);
 }
 
 /**
@@ -111,7 +114,24 @@ TEST_F(PitTest, SpawnBlockOutOfBounds)
  */
 TEST_F(PitTest, SpawnGarbage)
 {
-	FAIL();
+	RowCol combo_rc{1, 2};
+	RowCol chain_rc{3, 0};
+	auto& combo_gb = pit->spawn_garbage(combo_rc, 3, 1);
+	auto& chain_gb = pit->spawn_garbage(chain_rc, 6, 2);
+
+	EXPECT_TRUE(pit->at(combo_rc));
+	EXPECT_TRUE(pit->at(chain_rc));
+	EXPECT_EQ(&combo_gb, pit->garbage_at(RowCol{1, 4}));
+	EXPECT_EQ(&chain_gb, pit->garbage_at(RowCol{4, 5}));
+	EXPECT_TRUE(contains_n(*pit, 0, 2));
+
+	const char* content_str =
+		"      "
+		"  GGG "
+		"      "
+		"GGGGGG"
+		"GGGGGG";
+	EXPECT_EQ(0, contents_mismatch(*pit, content_str));
 }
 
 /**
@@ -119,7 +139,10 @@ TEST_F(PitTest, SpawnGarbage)
  */
 TEST_F(PitTest, SpawnGarbageOutOfBounds)
 {
-	FAIL();
+	RowCol combo_rc{1, -1};
+	RowCol chain_rc{3, 1};
+	EXPECT_THROW(pit->spawn_garbage(combo_rc, 3, 1), GameException);
+	EXPECT_THROW(pit->spawn_garbage(chain_rc, 6, 2), GameException);
 }
 
 /**
@@ -127,7 +150,12 @@ TEST_F(PitTest, SpawnGarbageOutOfBounds)
  */
 TEST_F(PitTest, CanFallBlockYes)
 {
-	FAIL();
+	RowCol red_rc{1, 2};
+	RowCol green_rc{2, 2};
+	pit->spawn_block(Block::Color::RED, red_rc, Block::State::REST);
+	auto& green_block = pit->spawn_block(Block::Color::GREEN, green_rc, Block::State::REST);
+
+	EXPECT_TRUE(pit->can_fall(green_block));
 }
 
 /**
@@ -135,7 +163,12 @@ TEST_F(PitTest, CanFallBlockYes)
  */
 TEST_F(PitTest, CanFallBlockNo)
 {
-	FAIL();
+	RowCol red_rc{1, 2};
+	RowCol green_rc{2, 2};
+	auto& red_block = pit->spawn_block(Block::Color::RED, red_rc, Block::State::REST);
+	pit->spawn_block(Block::Color::GREEN, green_rc, Block::State::REST);
+
+	EXPECT_FALSE(pit->can_fall(red_block));
 }
 
 /**
@@ -143,7 +176,12 @@ TEST_F(PitTest, CanFallBlockNo)
  */
 TEST_F(PitTest, CanFallGarbageYes)
 {
-	FAIL();
+	RowCol combo_rc{3, 2};
+	RowCol chain_rc{1, 0};
+	auto& combo_gb = pit->spawn_garbage(combo_rc, 3, 1);
+	pit->spawn_garbage(chain_rc, 6, 2);
+
+	EXPECT_TRUE(pit->can_fall(combo_gb));
 }
 
 /**
@@ -151,7 +189,12 @@ TEST_F(PitTest, CanFallGarbageYes)
  */
 TEST_F(PitTest, CanFallGarbageNo)
 {
-	FAIL();
+	RowCol combo_rc{3, 2};
+	RowCol chain_rc{1, 0};
+	pit->spawn_garbage(combo_rc, 3, 1);
+	auto& chain_gb = pit->spawn_garbage(chain_rc, 6, 2);
+
+	EXPECT_FALSE(pit->can_fall(chain_gb));
 }
 
 /**
@@ -159,7 +202,26 @@ TEST_F(PitTest, CanFallGarbageNo)
  */
 TEST_F(PitTest, FallBlock)
 {
-	FAIL();
+	RowCol red_rc{1, 2};
+	RowCol green_rc{3, 2};
+	auto& red_block = pit->spawn_block(Block::Color::RED, red_rc, Block::State::REST);
+	pit->spawn_block(Block::Color::GREEN, green_rc, Block::State::REST);
+
+	pit->fall(red_block);
+
+	EXPECT_FALSE(pit->at(red_rc));
+	red_rc.r++;
+	EXPECT_TRUE(pit->at(red_rc));
+	EXPECT_EQ(red_rc, red_block.rc());
+	EXPECT_TRUE(contains_n(*pit, 2, 0));
+
+	const char* content_str =
+		"      "
+		"      "
+		"  B   "
+		"  B   "
+		"      ";
+	EXPECT_EQ(0, contents_mismatch(*pit, content_str));
 }
 
 /**
@@ -167,7 +229,12 @@ TEST_F(PitTest, FallBlock)
  */
 TEST_F(PitTest, FallBlockFail)
 {
-	FAIL();
+	RowCol red_rc{2, 2};
+	RowCol green_rc{3, 2};
+	auto& red_block = pit->spawn_block(Block::Color::RED, red_rc, Block::State::REST);
+	pit->spawn_block(Block::Color::GREEN, green_rc, Block::State::REST);
+
+	EXPECT_THROW(pit->fall(red_block), GameException);
 }
 
 /**
@@ -175,7 +242,26 @@ TEST_F(PitTest, FallBlockFail)
  */
 TEST_F(PitTest, FallGarbage)
 {
-	FAIL();
+	RowCol combo_rc{4, 2};
+	RowCol chain_rc{1, 0};
+	pit->spawn_garbage(combo_rc, 3, 1);
+	auto& chain_gb = pit->spawn_garbage(chain_rc, 6, 2);
+
+	pit->fall(chain_gb);
+
+	EXPECT_FALSE(pit->at(chain_rc));
+	EXPECT_TRUE(pit->at(RowCol{3,3}));
+	chain_rc.r++;
+	EXPECT_EQ(chain_rc, chain_gb.rc());
+	EXPECT_TRUE(contains_n(*pit, 0, 2));
+
+	const char* content_str =
+		"      "
+		"      "
+		"GGGGGG"
+		"GGGGGG"
+		"  GGG ";
+	EXPECT_EQ(0, contents_mismatch(*pit, content_str));
 }
 
 /**
@@ -183,7 +269,12 @@ TEST_F(PitTest, FallGarbage)
  */
 TEST_F(PitTest, FallGarbageFail)
 {
-	FAIL();
+	RowCol combo_rc{3, 2};
+	RowCol chain_rc{1, 0};
+	pit->spawn_garbage(combo_rc, 3, 1);
+	auto& chain_gb = pit->spawn_garbage(chain_rc, 6, 2);
+
+	EXPECT_THROW(pit->fall(chain_gb), GameException);
 }
 
 /**
@@ -191,7 +282,28 @@ TEST_F(PitTest, FallGarbageFail)
  */
 TEST_F(PitTest, KillBlock)
 {
-	FAIL();
+	RowCol red_rc{1, 2};
+	RowCol green_rc{3, 2};
+	RowCol yellow_rc{2, 4};
+	auto& red_block = pit->spawn_block(Block::Color::RED, red_rc, Block::State::REST);
+	auto& green_block = pit->spawn_block(Block::Color::GREEN, green_rc, Block::State::REST);
+	pit->spawn_block(Block::Color::YELLOW, yellow_rc, Block::State::REST);
+
+	red_block.set_state(Physical::State::DEAD);
+	green_block.set_state(Physical::State::DEAD);
+	pit->remove_dead();
+	EXPECT_FALSE(pit->at(red_rc));
+	EXPECT_FALSE(pit->at(green_rc));
+	EXPECT_TRUE(pit->at(yellow_rc));
+	EXPECT_TRUE(contains_n(*pit, 1, 0));
+
+	const char* content_str =
+		"      "
+		"      "
+		"    B "
+		"      "
+		"      ";
+	EXPECT_EQ(0, contents_mismatch(*pit, content_str));
 }
 
 /**
@@ -199,7 +311,24 @@ TEST_F(PitTest, KillBlock)
  */
 TEST_F(PitTest, ShrinkGarbage)
 {
-	FAIL();
+	RowCol combo_rc{1, 2};
+	RowCol chain_rc{3, 0};
+	pit->spawn_garbage(combo_rc, 3, 1);
+	auto& chain_gb = pit->spawn_garbage(chain_rc, 6, 2);
+
+	pit->shrink(chain_gb);
+
+	EXPECT_TRUE(pit->at(chain_rc));
+	EXPECT_FALSE(pit->at(RowCol{4,1}));
+	EXPECT_TRUE(contains_n(*pit, 0, 2));
+
+	const char* content_str =
+		"      "
+		"  GGG "
+		"      "
+		"GGGGGG"
+		"      ";
+	EXPECT_EQ(0, contents_mismatch(*pit, content_str));
 }
 
 /**
@@ -207,5 +336,21 @@ TEST_F(PitTest, ShrinkGarbage)
  */
 TEST_F(PitTest, KillGarbage)
 {
-	FAIL();
+	RowCol combo_rc{1, 2};
+	RowCol chain_rc{3, 0};
+	auto& combo_gb = pit->spawn_garbage(combo_rc, 3, 1);
+	pit->spawn_garbage(chain_rc, 6, 2);
+
+	pit->shrink(combo_gb);
+
+	EXPECT_FALSE(pit->at(combo_rc));
+	EXPECT_TRUE(contains_n(*pit, 0, 1));
+
+	const char* content_str =
+		"      "
+		"      "
+		"      "
+		"GGGGGG"
+		"GGGGGG";
+	EXPECT_EQ(0, contents_mismatch(*pit, content_str));
 }
