b10f55a3a6c78dacff7f698d928880c2ee8b2991
Peter Neubauer <peterneubauer2@gmail.com>
2017-01-28 22:50:18 +0100

    Tests: fixes after refactoring

--- tests/test_block.cpp
+++ tests/test_block.cpp
@@ -3,7 +3,6 @@
  */
 
 #include "stage.hpp"
-#include "mock.hpp"
 #include "gtest/gtest.h"
 
 /**
@@ -12,7 +11,6 @@
 TEST(BlockTest, Fall)
 {
 	// setup
-	auto context = MockContext();
 	Block block(Block::Color::BLUE, RowCol{3,3}, Block::State::FALL);
 
 	Point loc0 = block.loc(); // current location
@@ -20,8 +18,8 @@ TEST(BlockTest, Fall)
 
 	for(int i = 0; i < TICKS; i++)
 	{
-		block.update(context);
+		block.update();
 	}
 
-	EXPECT_EQ(loc0.y + TICKS * FALL_SPEED, block.loc().y);
+	EXPECT_EQ(loc0.y + TICKS * ROW_H * FALL_SPEED / ROW_HEIGHT, block.loc().y);
 }
