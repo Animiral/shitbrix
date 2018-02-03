99caffa9fc99a2f2605fe9cddcd9aaed4e69286f
Peter Neubauer <peterneubauer2@gmail.com>
2018-02-03 07:24:39 +0100

    Keyboard: second keyset for right player
    Gave another mapping of keys to the right side player to improve my
    options for testing on keyboards without numpad.

--- src/input.cpp
+++ src/input.cpp
@@ -14,23 +14,30 @@ ControllerInput key_to_controller(SDL_Keycode key, Uint8 state)
 	Button button;
 
 	switch(key) {
+		// player 0 default keys
 		case SDLK_LEFT:  player = 0; button = Button::LEFT;  break;
 		case SDLK_RIGHT: player = 0; button = Button::RIGHT; break;
 		case SDLK_UP:    player = 0; button = Button::UP;    break;
 		case SDLK_DOWN:  player = 0; button = Button::DOWN;  break;
 		case SDLK_z:     player = 0; button = Button::A;     break;
 		case SDLK_x:     player = 0; button = Button::B;     break;
-		case SDLK_KP_4:  player = 1; button = Button::LEFT;  break;
-		case SDLK_KP_6:  player = 1; button = Button::RIGHT; break;
-		case SDLK_KP_8:  player = 1; button = Button::UP;    break;
-		case SDLK_KP_5:  player = 1; button = Button::DOWN;  break;
-		case SDLK_KP_0:  player = 1; button = Button::A;     break;
-		case SDLK_KP_1:  player = 1; button = Button::B;     break;
+
+		// player 1 default keys
+		case SDLK_KP_4: case SDLK_j: player = 1; button = Button::LEFT;  break;
+		case SDLK_KP_6: case SDLK_l: player = 1; button = Button::RIGHT; break;
+		case SDLK_KP_8: case SDLK_i: player = 1; button = Button::UP;    break;
+		case SDLK_KP_5: case SDLK_k: player = 1; button = Button::DOWN;  break;
+		case SDLK_KP_0: case SDLK_g: player = 1; button = Button::A;     break;
+		case SDLK_KP_1: case SDLK_h: player = 1; button = Button::B;     break;
+
+		// debug keys
 		case SDLK_F1:     button = Button::DEBUG1; break;
 		case SDLK_F2:     button = Button::DEBUG2; break;
 		case SDLK_F3:     button = Button::DEBUG3; break;
 		case SDLK_F4:     button = Button::DEBUG4; break;
 		case SDLK_F5:     button = Button::DEBUG5; break;
+
+		// control keys
 		case SDLK_RETURN: button = Button::RESET;  break;
 		case SDLK_SPACE:  button = Button::PAUSE;  break;
 		case SDLK_ESCAPE: button = Button::QUIT;   break;
